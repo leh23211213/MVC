3f334cc8d390ea091e683ad7761eace2e6989395
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2023-11-02 09:20:50 +0700

    update View folder

---
+++ Views/BookSingleView.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Views
+{
+    using Models; // chú ý  cách dùng namespace
+    public class BookSingleView
+    {
+        
+    }
+}
